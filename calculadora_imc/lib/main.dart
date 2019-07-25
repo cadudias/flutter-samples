b88ae4bf46441ff8e3a943331c99db108d2d2070
Ricardo Hofstetter <ricardo.hofstetter@redasset.com.br>
2019-07-25 13:48:45 -0300

    adicionar validação dos campos e caluclo do imc

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -10,6 +10,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = "Informe seus dados";
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+
+    setState(() {
+      _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>(); //remove as msg de erro no reset
+    });
+  }
+
+  void _calcularImc()
+  {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+
+      double imc = weight / (height * height);
+
+      if(imc < 18.5)
+      {
+        _infoText = "Abaixo do peso ${imc.toStringAsPrecision(4)}";
+      }else{
+        _infoText = "Peso ideal ${imc.toStringAsPrecision(4)}";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,52 +55,73 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(icon: Icon(Icons.refresh),
           onPressed: () {
-
+            _resetFields();
           })
         ],
       ) ,
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch, // ocupa toda a linha da coluna
-            children: <Widget>[
-              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-              TextField(
-                  keyboardType:  TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Peso (kg)",
-                      labelStyle: TextStyle(color: Colors.green)
+          child: Form(
+            key: _formKey,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch, // ocupa toda a linha da coluna
+                children: <Widget>[
+                  Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+                  TextFormField(
+                    keyboardType:  TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Peso (kg)",
+                        labelStyle: TextStyle(color: Colors.green)
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: weightController,
+                    validator: (value)
+                    {
+                      if(value.isEmpty) {
+                        return "Insira seu peso";
+                      }
+                    },
                   ),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0)
-              ),
-              TextField(
-                  keyboardType:  TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Altura (cm)",
-                      labelStyle: TextStyle(color: Colors.green)
+                  TextFormField(
+                    keyboardType:  TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Altura (cm)",
+                        labelStyle: TextStyle(color: Colors.green)
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: heightController,
+                    validator: (value)
+                    {
+                      if(value.isEmpty) {
+                        return "Insira sua altura";
+                      }
+                    },
                   ),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0)
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                child: Container(
-                    height: 50.0,
-                    child: RaisedButton(
-                        onPressed: () {},
-                        child: Text("Calcular",
-                            style: TextStyle(color: Colors.white, fontSize: 25.0)),
-                        color: Colors.green
-                    )
-                )
-              ),
-              Text("Info",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0))
-            ]
-        )
+                  Padding(
+                      padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: Container(
+                          height: 50.0,
+                          child: RaisedButton(
+                              onPressed: () {
+                                if(_formKey.currentState.validate()) {
+                                  _calcularImc();
+                                }},
+                              child: Text("Calcular",
+                                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                              color: Colors.green
+                          )
+                      )
+                  ),
+                  Text(
+                      _infoText,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green, fontSize: 25.0))
+                ]
+            ),
+          )
       )
     );
   }
